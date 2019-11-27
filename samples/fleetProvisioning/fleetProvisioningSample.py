1f4029823cf10ae98e5f44f052e1341098152145
Ranganath <rangam@amazon.com>
2019-11-27 10:32:48 -0800

    Adding websocket support.

--- samples/fleetProvisioning/fleetProvisioningSample.py
+++ samples/fleetProvisioning/fleetProvisioningSample.py
@@ -147,10 +147,13 @@ class FleetProvisioningProcessor(object):
 parser = argparse.ArgumentParser()
 parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
 parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
-parser.add_argument("-c", "--cert", action="store", dest="certificatePath", required=True, help="Certificate file path")
-parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", required=True, help="Private key file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
 parser.add_argument("-t", "--templateName", action="store", required=True, dest="templateName", help="Template name")
-parser.add_argument("-p", "--parameters", action="store", dest="templateParameters", help="Template Parameter")
+parser.add_argument("-tp", "--templateParameters", action="store", dest="templateParameters", help="Values for Template Parameters")
+parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
 parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="FleetProvisioningPythonSample",
                     help="Targeted client id")
 
@@ -160,9 +163,25 @@ rootCAPath = args.rootCAPath
 certificatePath = args.certificatePath
 privateKeyPath = args.privateKeyPath
 clientId = args.clientId
+port = args.port
+useWebsocket = args.useWebsocket
 templateName = args.templateName
 templateParameters = json.loads(args.templateParameters)
-port = 8883
+
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Port defaults
+if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
+    port = 443
+if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
+    port = 8883
 
 # Configure logging
 logger = logging.getLogger("AWSIoTPythonSDK.core")
@@ -174,9 +193,14 @@ logger.addHandler(streamHandler)
 
 # Init AWSIoTMQTTClient
 myAWSIoTMQTTClient = None
-myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-myAWSIoTMQTTClient.configureEndpoint(host, port)
-myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
 # AWSIoTMQTTClient connection configuration
 myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
